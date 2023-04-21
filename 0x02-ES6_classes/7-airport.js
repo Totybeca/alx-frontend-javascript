407df26d06e880304611c5ae59edba54224b083e
Totybeca <emeraldtoyin@gmail.com>
2023-04-21 01:21:42 +0000

    Airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,27 +1,32 @@
 export default class Airport {
-    constructor(name, code) {
-      
-      this._code = code;
-      this._name = name;
-    }
-  
-   get [Symbol.toStringTag]() {
-      return `${this._code}`;
-    }
-  
-    set code(newCode) { 
-      this._code = newCode;
-    }
-  
-    set name(newName) { 
-      this._name = newName;
-    }
-  
-    get code() { 
-      return this._code;
-    }
-  
-    get name() { 
-      return this._name;
-    }
+  constructor(name, code) {
+    // Create objs
+    this._code = code;
+    this._name = name;
   }
+
+  // Method
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+
+  // Setters for code and name attributes for the class
+  set code(newCode) { // Setter for code attribute (string)
+    this._code = newCode;
+  }
+
+  set name(newName) { // Setter for name attribute (string)
+    this._name = newName;
+  }
+
+  // Getters for code and name attributes for the class
+  get code() { // Getter for code attribute (string)
+    return this._code;
+  }
+
+  get name() { // Getter for name attribute (string)
+    return this._name;
+  }
+}
+
