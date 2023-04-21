5346535404ed1f8b3ca510517292168ba29948b5
Totybeca <emeraldtoyin@gmail.com>
2023-04-21 00:12:33 +0000

    classroom

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,9 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms(){
-	return [
-		new classRoom(19),
-		new classRoom(20),
-		new classRoom(34),
-	];
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
