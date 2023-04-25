cc706b5c459c5e896a058267d68ca99b36165741
Totybeca <emeraldtoyin@gmail.com>
2023-04-25 22:04:08 +0000

    filter and map

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,8 @@ export default  function updateStudentGradeByCity(students, city, newGrade) {
     return students
     .filter((student) => student.location === city)
     .map((student) => {
-        const grade1 = newGrade.find((grade2) => grade2.studentId === student.id);
-        const grade2 = grade1 ? grade1.grade2 : 'N/A';
-        return {...student, grade2 };
+        const grades1 = newGrade.find((grade) => grade.studentId === student.id);
+        const grade = grades1 ? grades1.grade: 'N/A';
+        return { ...student, grade };
     });
 }
