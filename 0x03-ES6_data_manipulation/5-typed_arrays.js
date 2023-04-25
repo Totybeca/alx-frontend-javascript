057601eeb3882ac9b1c1dc2fe6b846a3609d5258
Totybeca <emeraldtoyin@gmail.com>
2023-04-25 23:11:17 +0000

    typed_arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,6 +1,6 @@
 export default function createInt8TypedArray(length, position, value) {
-    const buff = new ArrayBuffer(length);
-    const dataView = new DataView(buff);
+    const buffer = new ArrayBuffer(length);
+    const dataView = new DataView(buffer);
     if (position >= length) throw new RangeError('Position outside range');
     dataView.setUint8(position, value);
     return dataView;
