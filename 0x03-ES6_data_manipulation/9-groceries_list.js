45dba9e7a94a91a71abb164422900a70c0f0e28c
Totybeca <emeraldtoyin@gmail.com>
2023-04-26 00:26:29 +0000

    map

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,10 +1,10 @@
 export default function groceriesList() {
-    const lists = new Map();
-    lists.set('Apples', 10);
-    lists.set('Tomatoes', 10);
-    lists.set('Pasta', 1);
-    lists.set('Rice', 1);
-    lists.set('Banana', 5);
+    const groceries = new Map();
+    groceries.set('Apples', 10);
+    groceries.set('Tomatoes', 10);
+    groceries.set('Pasta', 1);
+    groceries.set('Rice', 1);
+    groceries.set('Banana', 5);
   
-    return lists;
+    return groceries;
   }
